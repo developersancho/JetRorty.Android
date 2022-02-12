979b04acbb9da9fda044262ad885c7977961e830
Mr.Sanchez <developersancho@gmail.com>
2022-02-13 00:47:01 +0300

    [DEV][Changed ThemeSwitch.kt icon colors]

--- common/ui-view/src/main/java/com/developersancho/ui/view/ThemeSwitch.kt
+++ common/ui-view/src/main/java/com/developersancho/ui/view/ThemeSwitch.kt
@@ -3,13 +3,13 @@ package com.developersancho.ui.view
 import android.content.Context
 import android.util.AttributeSet
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.appcompat.widget.SwitchCompat
 import com.developersancho.ui.resource.R
-import com.google.android.material.switchmaterial.SwitchMaterial
 
 class ThemeSwitch @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null
-) : SwitchMaterial(context, attrs) {
+) : SwitchCompat(context, attrs) {
 
     init {
         thumbDrawable = AppCompatResources.getDrawable(context, R.drawable.selector_dark_light)
